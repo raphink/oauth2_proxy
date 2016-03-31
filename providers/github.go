f4730ad7501fc7654df356eb7a15d217d935adb9
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-03-31 15:46:14 +0200

    Do not return emails in github

--- providers/github.go
+++ providers/github.go
@@ -165,54 +165,5 @@ func (p *GitHubProvider) hasOrgAndTeam(accessToken string) (bool, error) {
 }
 
 func (p *GitHubProvider) GetEmailAddress(s *SessionState) (string, error) {
-
-	var emails []struct {
-		Email   string `json:"email"`
-		Primary bool   `json:"primary"`
-	}
-
-	// if we require an Org or Team, check that first
-	if p.Org != "" {
-		if p.Team != "" {
-			if ok, err := p.hasOrgAndTeam(s.AccessToken); err != nil || !ok {
-				return "", err
-			}
-		} else {
-			if ok, err := p.hasOrg(s.AccessToken); err != nil || !ok {
-				return "", err
-			}
-		}
-	}
-
-	params := url.Values{
-		"access_token": {s.AccessToken},
-	}
-	endpoint := p.ValidateURL.Scheme + "://" + p.ValidateURL.Host + p.ValidateURL.Path + "?" + params.Encode()
-	resp, err := http.DefaultClient.Get(endpoint)
-	if err != nil {
-		return "", err
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return "", err
-	}
-
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("got %d from %q %s", resp.StatusCode, endpoint, body)
-	} else {
-		log.Printf("got %d from %q %s", resp.StatusCode, endpoint, body)
-	}
-
-	if err := json.Unmarshal(body, &emails); err != nil {
-		return "", fmt.Errorf("%s unmarshaling %s", err, body)
-	}
-
-	for _, email := range emails {
-		if email.Primary {
-			return email.Email, nil
-		}
-	}
-
 	return "", nil
 }
